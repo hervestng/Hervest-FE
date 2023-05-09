b97e470022c08142cb08359c66ccc9ff25ed4ed5
Yomi <abayomi.ogunleye@gmail.com>
2023-05-09 15:45:51 +0100

    Update ApprovedCampuses.vue

--- components/includes/campus/ApprovedCampuses.vue
+++ components/includes/campus/ApprovedCampuses.vue
@@ -25,7 +25,7 @@
       </div>
       <div class="apply-line" />
       <div class="apply-button-block">
-        <a class="button campus w-button" href="https://forms.office.com/r/Zg3iRR91RY" target="_blank">{{ widgetData.cta_text }}
+        <a class="button campus w-button" href="https://forms.office.com/r/Zg3iRR91RY" target="_blank">Apply Here
         </a>
         <!-- <nuxt-link to="/contact" class="button campus w-button">
           {{ widgetData.cta_text }}
